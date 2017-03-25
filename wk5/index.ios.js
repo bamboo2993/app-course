22e13d82d5b56a425308d23e473a6ae261fa75f8
bamboo2993 <work2993@gmail.com>
2017-03-25 21:41:02 +0800

    Add files via upload

---
+++ wk5/index.ios.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  View,
+} from 'react-native';
+
+//import Page from './page1.js';
+import Page from './page2.js';
+//import Page from './page3.js';
+
+const wk5 = () => (
+
+   
+    <Page />
+
+
+
+)
+
+
+
+
+AppRegistry.registerComponent('wk5', () => wk5);
